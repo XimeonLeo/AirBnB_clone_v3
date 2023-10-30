6cbd82a3525a5655c31dbd0484387c8e908b17af
Samuel Ogboye <ogboyesam@gmail.com>
2023-10-30 09:39:37 +0100

    Fixed indentation issues

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -12,72 +12,72 @@ from os import environ
 @app_views.route('/places/<place_id>/amenities',
                  methods=['GET'], strict_slashes=False)
 def get_amenities(place_id):
-    """
-    Get the list of amenities for a given place.
-    """
-    place = storage.get(Place, place_id)
-    if not place:
-        abort(404, description="Place not found")
+        """
+        Get the list of amenities for a given place.
+        """
+        place = storage.get(Place, place_id)
+        if not place:
+                abort(404, description="Place not found")
 
-    if environ.get('HBNB_TYPE_STORAGE') == 'db':
-        amenities = [amenity.to_dict() for amenity in place.amenities]
-    else:
-        amenities = [storage.get(Amenity, amenity_id).to_dict()
-                     for amenity_id in place.amenity_ids]
+        if environ.get('HBNB_TYPE_STORAGE') == 'db':
+                amenities = [amenity.to_dict() for amenity in place.amenities]
+        else:
+            amenities = [storage.get(Amenity, amenity_id).to_dict()
+                         for amenity_id in place.amenity_ids]
 
-    return jsonify(amenities), 200
+        return jsonify(amenities), 200
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['DELETE'], strict_slashes=False)
 def delete_amenity(place_id, amenity_id):
-    """
-    Deletes a amenity from the place.
-    """
-    place = storage.get(Place, place_id)
-    if not place:
-        abort(404, description="Place not found")
+        """
+        Deletes a amenity from the place.
+        """
+        place = storage.get(Place, place_id)
+        if not place:
+                abort(404, description="Place not found")
 
-    amenity = storage.get(Amenity, amenity_id)
+        amenity = storage.get(Amenity, amenity_id)
 
-    if not amenity:
-        abort(404, description="Amenity not found")
+        if not amenity:
+                abort(404, description="Amenity not found")
 
-    if environ.get('HBNB_TYPE_STORAGE') == 'db':
-        if amenity not in place.amenities:
-            abort(404, description="Amenity not in place")
-        place.amenities.remove(amenity)
-    else:
-        if amenity not in place.amenity_id:
-            abort(404, description="Amenity not in place")
-        place.amenities.remove(amenity_id)
+        if environ.get('HBNB_TYPE_STORAGE') == 'db':
+                if amenity not in place.amenities:
+                        abort(404, description="Amenity not in place")
+                place.amenities.remove(amenity)
+        else:
+                if amenity not in place.amenity_id:
+                        abort(404, description="Amenity not in place")
+                place.amenities.remove(amenity_id)
 
-    storage.save()
-    return jsonify({}), 200
+        storage.save()
+        return jsonify({}), 200
 
 
 @app_views.route('/places/<place_id>/amenities/<amenity_id>',
                  methods=['POST'], strict_slashes=False)
 def post_amenity(place_id, amenity_id):
-    """
-    Adds an amenity to a place.
-    """
-    place = storage.get(Place, place_id)
-    if not place:
-        abort(404, description="Place not found")
-    amenity = storage.get(Amenity, amenity_id)
-    if not amenity:
-        abort(404, description="Amenity not found")
-    if environ.get('HBNB_TYPE_STORAGE') == 'db':
-        if amenity in place.amenities:
-            return jsonify(amenity.to_dict()), 200
+        """
+        Adds an amenity to a place.
+        """
+        place = storage.get(Place, place_id)
+        if not place:
+                abort(404, description="Place not found")
+                amenity = storage.get(Amenity, amenity_id)
+        if not amenity:
+                abort(404, description="Amenity not found")
+        if environ.get('HBNB_TYPE_STORAGE') == 'db':
+                if amenity in place.amenities:
+                        return jsonify(amenity.to_dict()), 200
+                else:
+                        place.amenities.append(amenity)
         else:
-            place.amenities.append(amenity)
-    else:
-        if amenity in place.amenity_ids:
-            return jsonify(amenity.to_dict()), 200
-        else:
-            place.amenity_ids.append(amenity_id)
+                if amenity in place.amenity_ids:
+                        return jsonify(amenity.to_dict()), 200
+                else:
+                        place.amenity_ids.append(amenity_id)
 
-    storage.save()
-    return jsonify(amenity.to_dict()), 201
+        storage.save()
+        return jsonify(amenity.to_dict()), 201
